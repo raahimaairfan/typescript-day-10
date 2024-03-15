3cdbed6b5f1e1ec88eb75d8c7f92f0355250dccf
raahimaairfan <133574668+raahimaairfan@users.noreply.github.com>
2024-03-15 22:34:25 +0500

    Add files via upload

---
+++ 29.ts
@@ -0,0 +1,24 @@
+/* Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent 
+if statements that check for certain fruits in your array.
+
+* Make a array of your three favorite fruits and call it favorite_fruits.
+* Write five if statements. Each should check whether a certain kind of fruit is in your array. 
+If the fruit is in your array, the if block should print a statement, such as You really like bananas! */
+
+let favorite_fruits: string[] = ["apples", "bananas", "cherries"];
+
+if (favorite_fruits.indexOf("bananas") !== -1) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.indexOf("apples") !== -1) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.indexOf("cherries") !== -1) {
+    console.log("You really like cherries!");
+}
+if (favorite_fruits.indexOf("strawberry") !== -1) {
+    // Do something if "strawberry" is found
+}
+if (favorite_fruits.indexOf("guava") !== -1) {
+    // Do something if "guava" is found
+}
